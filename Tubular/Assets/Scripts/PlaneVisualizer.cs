694fe4034e0045f022ce4b9981421df862094161
The Stick <avaera8820@gmail.com>
2015-10-03 21:58:52 -0400

    Gave the player legs.  I think.  It doesn't render.
    Also changed the world a little bit.

---
+++ Tubular/Assets/Scripts/PlaneVisualizer.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[AddComponentMenu("Tubular Scripts/In-Editor/Plane Visualizer")]
+public class PlaneVisualizer : MonoBehaviour {
+    public Vector2 PlaneSize;
+
+    void OnDrawGizmos() {
+        Gizmos.DrawWireCube(transform.position + new Vector3(0f, PlaneSize.y * 0.5f, 0f), new Vector3(PlaneSize.x, PlaneSize.y, 0.001f));
+    }
+}
---
+++ Tubular/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+[AddComponentMenu("Tubular Scripts/Runtime/Networked/Player Controller")]
+public class PlayerController : NetworkBehaviour {
+    private Rigidbody rb;
+    public LayerMask whatIsGround;
+    public float PlayerMoveSpeed = 10f;
+
+    private bool isGrounded {
+        get {
+            return Physics.CheckSphere(transform.position + (Vector3.up * -1.25f), 0.5f, whatIsGround);
+        }
+    }
+
+
+    void Update() {
+        if (rb == null) rb = GetComponent<Rigidbody>();
+        rb.AddForce(Input.GetAxis("Horizontal") * PlayerMoveSpeed, 0f, 0f, ForceMode.VelocityChange);
+
+
+        if (Input.GetButtonDown("Jump") && isGrounded) {
+            rb.AddRelativeForce(0f, 10f, 0f, ForceMode.Impulse);
+        }
+    }
+}
