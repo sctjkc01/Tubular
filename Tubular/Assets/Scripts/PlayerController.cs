2e3ee955751bc2cdcbfe8ab44d05130d968a450f
fad4470 <fad4470@rit.edu>
2015-10-04 21:17:06 -0400

    Re-added local player test Removed for testing, forgot to replace.

--- Tubular/Assets/Scripts/PlayerController.cs
+++ Tubular/Assets/Scripts/PlayerController.cs
@@ -18,7 +18,7 @@ public class PlayerController : NetworkBehaviour {
     void Update() {
         if (rb == null) rb = GetComponent<Rigidbody>();
 
-        if (/*isLocalPlayer && */isGrounded) {
+        if (isLocalPlayer && isGrounded) {
             rb.velocity = new Vector3(Input.GetAxis("Horizontal") * PlayerMoveSpeed, rb.velocity.y, 0f);
 
             if (Input.GetButtonDown("Jump")) {
