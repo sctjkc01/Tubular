6322edc06c7e9047c9efd8ce929a4cb9641b8119
The Stick <avaera8820@gmail.com>
2015-10-07 12:00:41 -0400

    Obstacles have been given a thruster!
    Use GameManager -> Game Travel Speed to change how quickly it moves.

---
+++ Tubular/Assets/Scripts/Management/GameManager.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+    /// <summary>
+    /// Value read by everything that travels.  Value is in Unity Units per Second.
+    /// </summary>
+    [Tooltip("In Unity Units per Second.  Read by everything that travels.")]
+    public float GameTravelSpeed = 5.0f;
+
+    public static GameManager inst;
+
+    void Awake() {
+        if (inst == null) inst = this;
+        else Destroy(this.gameObject);
+    }
+}
---
+++ Tubular/Assets/Scripts/Objects/ObjectSpawner.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObstacleSpawner : MonoBehaviour {
+    /// <summary>
+    /// List of all object bundles that will be chosen from to spawn.
+    /// </summary>
+    public SpawnBundle[] bundles;
+
+    /// <summary>
+    /// A bundle of things that can be spawned.
+    /// </summary>
+    [System.Serializable]
+    public struct SpawnBundle {
+        /// <summary>
+        /// The bundle to spawn.
+        /// </summary>
+        public GameObject prefab;
+        /// <summary>
+        /// How far from the center can this bundle be?
+        /// </summary>
+        public float maxDistFromCenter;
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+}
