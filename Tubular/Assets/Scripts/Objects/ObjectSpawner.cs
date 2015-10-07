bc5ec2b31d956f55a8ee30089480b1cff1aa5d23
The Stick <avaera8820@gmail.com>
2015-10-07 12:45:42 -0400

    Obstacle spawning!

--- Tubular/Assets/Scripts/Objects/ObjectSpawner.cs
+++ Tubular/Assets/Scripts/Objects/ObjectSpawner.cs
@@ -1,11 +1,20 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class ObstacleSpawner : MonoBehaviour {
+public class ObjectSpawner : MonoBehaviour {
     /// <summary>
     /// List of all object bundles that will be chosen from to spawn.
     /// </summary>
     public SpawnBundle[] bundles;
+
+    /// <summary>
+    /// Chance per second that somsthing will spawn.
+    /// </summary>
+    [Tooltip("Chance per second that something will spawn, out of 1")]
+    [Range(0f, 1f)]
+    public float SpawnChance = 0.01f;
+
+    private float TimeSinceLastCheck = 0f;
 
     /// <summary>
     /// A bundle of things that can be spawned.
@@ -24,6 +33,19 @@ public class ObstacleSpawner : MonoBehaviour {
 
     // Update is called once per frame
     void Update() {
+        TimeSinceLastCheck += Time.deltaTime;
 
+        if(TimeSinceLastCheck > 1.0f) {
+            while (TimeSinceLastCheck > 1.0f) {
+                if (Random.value < SpawnChance) {
+                    SpawnBundle bundleToUse = bundles[Random.Range(0, bundles.Length)];
+                    GameObject newGO = GameObject.Instantiate<GameObject>(bundleToUse.prefab);
+                    newGO.transform.SetParent(transform);
+                    newGO.transform.localPosition = new Vector3(Random.Range(bundleToUse.maxDistFromCenter * -1f, bundleToUse.maxDistFromCenter), 0, 0);
+                }
+
+                TimeSinceLastCheck -= 1.0f;
+            }
+        }
     }
 }
