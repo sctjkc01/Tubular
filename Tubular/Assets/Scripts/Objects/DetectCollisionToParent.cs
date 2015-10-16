04a581827c3bf9075706427b291f656f152b6405
fad4470 <fad4470@rit.edu>
2015-10-16 13:50:14 -0400

    Added collision tracking for obstacles.

---
+++ Tubular/Assets/Scripts/Objects/DetectCollisionToParent.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DetectCollisionToParent : MonoBehaviour {
+
+    void OnTriggerEnter(Collider col)
+    {
+        this.SendMessageUpwards("OnTriggerEnterChild", col, SendMessageOptions.DontRequireReceiver);
+    }
+    void OnTriggerExit(Collider col)
+    {
+        this.SendMessageUpwards("OnTriggerExitChild", col, SendMessageOptions.DontRequireReceiver);
+    }
+}
--- Tubular/Assets/Scripts/PlayerController.cs
+++ Tubular/Assets/Scripts/PlayerController.cs
@@ -6,9 +6,9 @@ using UnityEngine.Networking;
 public class PlayerController : NetworkBehaviour {
     private Rigidbody rb;
     [SyncVar]
-    public bool alive;
+    public bool alive = true;
     public LayerMask whatIsGround;
-    public LayerMask whatIsObstacle;
+    //public LayerMask whatIsObstacle;
     public float PlayerMoveSpeed = 10f;
 
     private bool isGrounded {
@@ -16,16 +16,16 @@ public class PlayerController : NetworkBehaviour {
             return Physics.CheckSphere(transform.position + (Vector3.up * -1f), 0.25f, whatIsGround);
         }
     }
-    private bool foundObstacle {
+    /*private bool foundObstacle {
         get {
             return Physics.CheckSphere(transform.position + (Vector3.forward * 1f), 0.25f, whatIsObstacle);
         }
-    }
+    }*/
 
     void Start() {
         if(rb == null) rb = GetComponent<Rigidbody>();
 
-        rb.useGravity = isLocalPlayer;
+        rb.isKinematic = !isLocalPlayer;
 
     }
 
@@ -33,16 +33,40 @@ public class PlayerController : NetworkBehaviour {
     void Update() {
         if(rb == null) rb = GetComponent<Rigidbody>();
 
-        if(isLocalPlayer && isGrounded) {
-            rb.velocity = new Vector3(Input.GetAxis("Horizontal") * PlayerMoveSpeed, rb.velocity.y, 0f);
+        if (isLocalPlayer) {
+            if (!alive) { //Ghost controls
+                this.transform.position = new Vector3(this.transform.position.x, 15, this.transform.position.z);
+                rb.velocity = new Vector3(Input.GetAxis("Horizontal") * PlayerMoveSpeed, rb.velocity.y, 0f);
+            }
+            else if(isGrounded) //Alive controls
+            {
+                rb.velocity = new Vector3(Input.GetAxis("Horizontal") * PlayerMoveSpeed, rb.velocity.y, 0f);
 
-            if(Input.GetButtonDown("Jump")) {
-                rb.AddRelativeForce(0f, 20f, 0f, ForceMode.Impulse);
+                if (Input.GetButtonDown("Jump"))
+                {
+                    rb.AddRelativeForce(0f, 20f, 0f, ForceMode.Impulse);
+                }
             }
         }
 
-        if(isLocalPlayer && alive && foundObstacle) {
+        if(isLocalPlayer && !alive/* && foundObstacle*/) {
 
+        }
+    }
+
+    public void Collision(GameObject go, bool hostile)
+    {
+        if (hostile)
+        {
+            alive = false;
+            rb.useGravity = false;
+            rb.drag = 0.85f;
+            //Kill player, activate ghost mode
+        }
+        else
+        {
+            //Add points and multiplier
+            //TODO change hostile to enum representing type e.g. gate, obstacle, powerup
         }
     }
 }
