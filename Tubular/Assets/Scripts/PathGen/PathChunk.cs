127c35984b08804b30fd516ef860657a3a7f70dc
The Stick <avaera8820@gmail.com>
2015-10-26 13:38:06 -0400

    Path now generates, just need to get the camera to follow it.
    ... as well as get it to generate infinitely.

--- Tubular/Assets/Scripts/PathGen/PathChunk.cs
+++ Tubular/Assets/Scripts/PathGen/PathChunk.cs
@@ -2,5 +2,15 @@
 using System.Collections;
 
 public class PathChunk : MonoBehaviour {
+    public PathNode StartPoint {
+        get {
+            return transform.GetComponentInChildren<PathChunkEntry>();
+        }
+    }
 
+    public PathNode EndPoint {
+        get {
+            return transform.GetComponentInChildren<PathChunkExit>();
+        }
+    }
 }
