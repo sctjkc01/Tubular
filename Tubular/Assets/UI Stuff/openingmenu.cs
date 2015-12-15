b72a23d74297524c8eb15e2cb4f42b7f5fe86c14
unknown <sgr3495@GDD-22.main.ad.rit.edu>
2015-12-15 18:47:51 -0500

    working start up screen and lobby

--- Tubular/Assets/UI Stuff/openingmenu.cs
+++ Tubular/Assets/UI Stuff/openingmenu.cs
@@ -12,29 +12,12 @@ public class openingmenu : MonoBehaviour
 	public GameObject credits_panel;
 	private List<GameObject> somepanels = new List<GameObject>();
 
-	private List<Texture> arrayofimages = new List<Texture> ();
-	public Texture bgimage_1;
-	public Texture bgimage_2;
-	private int imgindex;
-
-	private RawImage img;
-
 	// Use this for initialization
 	void Start () 
 	{
 		somepanels.Add ((GameObject)login_panel);
 		somepanels.Add ((GameObject)instructions_panel);
 		somepanels.Add ((GameObject)credits_panel);
-		arrayofimages.Add ((Texture)bgimage_1);
-		arrayofimages.Add ((Texture)bgimage_2);
-		imgindex = 0;
-	}
-	
-	// Update is called once per frame
-	void Update () 
-	{
-		img = (RawImage)rawimage_panel.GetComponent<RawImage> ();
-		img.texture = (Texture)arrayofimages[imgindex];
 	}
 
 	public void loadtitle()
@@ -44,33 +27,20 @@ public class openingmenu : MonoBehaviour
 		{
 			somepanels[i].SetActive(false);
 		}
-		nextimage ();
 	}
 	public void loadlogin()
 	{
 		login_panel.SetActive (true);
 		title_panel.SetActive (false);
-		nextimage ();
 	}
 	public void loadinstructions()
 	{
 		instructions_panel.SetActive(true);
 		title_panel.SetActive(false);
-		nextimage();
 	}
 	public void loadcredits()
 	{
 		credits_panel.SetActive (true);
 		title_panel.SetActive (false);
-		nextimage ();
-	}
-
-	public void nextimage()
-	{
-		imgindex ++;
-		if(imgindex > arrayofimages.Count-1)
-		{
-			imgindex = 0;
-		}
 	}
 }
